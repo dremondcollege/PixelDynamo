cbfcf19eb2ad009e3a0c2f5484a103b14bd3c080
Stephen Coleridge <dremondcollege@gmail.com>
2025-07-01 12:58:52 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'TotalAV User Help'
+project = 'TotalAV Login Site'
 copyright = '2025'
-author = 'Rosemary Dinnage'
+author = 'Stephen Coleridge'
 
 release = '1.0'
 
